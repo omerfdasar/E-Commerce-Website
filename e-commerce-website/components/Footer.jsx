d94dd8ef7144b5edf003434832ec5ba2f4d2d216
Omer Dasar <omerfarukdasar@gmail.com>
2022-09-13 13:53:45 +0200

    clarified

--- e-commerce-website/components/Footer.jsx
+++ e-commerce-website/components/Footer.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import React from "react";
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
-  const date = new Date()
-  const year = date.getFullYear()
+  const date = new Date();
+  const year = date.getFullYear();
 
   return (
     <div className="footer-container">
       <p>{year} Omer Headphones All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://www.linkedin.com/in/omer-dasar/" target="_blank">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/omerfdasar" target="_blank">
+          <AiOutlineGithub />
+        </a>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
